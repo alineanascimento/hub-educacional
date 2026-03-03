8994c1aa5fccc9489598d8773c39a4d5390b68b1
alineanascimento <alineacioly@hotmail.com>
2026-03-02 21:15:31 -0300

    feat: integração com Anthropic API no Smart Assist

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from app.controllers.material_controller import MaterialRouter
 from app.database.database import Base, engine
-
+from app.controllers.ai_controller import AIRouter
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Hub Educacional")
 
 app.include_router(MaterialRouter)
+app.include_router(AIRouter)
